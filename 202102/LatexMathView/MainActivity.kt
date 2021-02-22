7d0d336ea0b421156bdd6e8c75977b3332fc2183
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2021-02-23 00:13:37 +0900

    Create MainActivity.kt

---
+++ 202102/LatexMathView/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sample.test.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        j_latex_math_view.setLatex("""
+            x = \frac{-b \pm \sqrt{b^2-4ac}} {2a}
+        """)
+     }
+}
