be635a11e77a822ab84f249823378f7a37c4035a
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2021-03-25 01:55:58 +0900

    Update MainActivity.kt

--- 202103/ListView/MainActivity.kt
+++ 202103/ListView/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
         findViewById<ListView>(R.id.listView).adapter = adapter
     }
 
